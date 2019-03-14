8ef78b8cdb5840e0c296629700be4466e9306876
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2019-03-14 10:42:25 +0100

    implementing fav list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lunchmaster3000-ui';
+export class AppComponent implements AfterViewInit {
+  
+  @ViewChild('inpname') inpname;
+
+  name: string = "Test";
+
+  locations = [{
+      name: "Green Thai",
+      link: "https://ni.greenthai.de",
+      votes: ["Thorsten", "Johnny"]
+    },
+    {
+      name: "City Döner",
+      link: "n/a",
+      votes: ["Klaus"]
+    },
+    {
+      name: "Subway",
+      link: "https://www.subway.com/de-DE"
+    },
+    {
+      name: "LSG Kantine",
+      link: "http://lz-catering.signage-server.de/frankfurt-neu-isenburg/web-app"
+    }
+  ]
+
+  ngAfterViewInit(): void {
+    setTimeout(()=>this.inpname.nativeElement.focus(),100);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 // Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { LocationComponent } from './location';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     // Material
     MatToolbarModule,
     MatCardModule,
-    MatInputModule
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
