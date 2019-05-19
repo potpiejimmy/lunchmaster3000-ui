f9eab82dc92b4b04f316f8d1fd7163f49c7ee122
potpiejimmy <t.liese@outlook.com>
2019-05-19 22:56:01 +0200

    stylings, added TypeWriter

--- src/app/components/topbar.ts
+++ src/app/components/topbar.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TypeWriter } from '../util/TypeWriter';
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './topbar.html'
 })
-export class AppTopbarComponent {
+export class AppTopbarComponent implements OnInit {
 
+    title: string;
+
+    ngOnInit() {
+        let tw = new TypeWriter(["„Was essen wir heute Mittag?“","lunch.community"], t => {
+            this.title = t;
+        })
+        tw.start();
+    }
 }
