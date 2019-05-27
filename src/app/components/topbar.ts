498537a1d27e57173db5e2f579823313780ba8b2
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2019-05-27 21:36:33 +0200

    implementing add/edit locations...

--- src/app/components/topbar.ts
+++ src/app/components/topbar.ts
@@ -24,7 +24,7 @@ export class AppTopbarComponent implements OnInit {
     }
 
     linkCopied() {
-        this.snackBar.open("Community link copied to clipboard", null, {duration: 3000});
+        this.snackBar.open("⎘ Community link copied to clipboard", null, {duration: 3000});
     }
 
     get communityLink(): string {
