ffa035822330491bb5947c6b4f8cd7d9799543a2
nixer89 <pbnixer@gmail.com>
2019-06-06 17:29:37 +0200

    implemented internationalization. currently only german supported. english translation needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +8,14 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    constructor(private translate: TranslateService) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('en');
+        
+        if (environment.supportedLanguages.includes(translate.getBrowserCultureLang())){
+            this.translate.use(translate.getBrowserCultureLang());
+        } else if (environment.supportedLanguages.includes(translate.getBrowserLang())){
+            this.translate.use(translate.getBrowserLang());
+        }
+    }
 }
