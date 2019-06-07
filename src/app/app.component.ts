adab9d56f813aa353a82220494d70ea2cf11bc27
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2019-06-07 10:24:17 +0200

    enhanced fav icon fixed google privacy links scroll to top on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    
+    constructor(
+        private router: Router
+    ) {}
+
+    ngOnInit() {
+        // always scroll to the top of the page on route change:
+        this.router.events.subscribe(e => e instanceof NavigationEnd ? window.scrollTo(0,0) : null);
+    }
 }
