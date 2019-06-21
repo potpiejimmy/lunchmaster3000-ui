d47f56680d2e4f60841b4cec7ff4950371a4e44b
potpiejimmy <t.liese@outlook.com>
2019-06-21 23:28:49 +0200

    English Privacy Policy

--- src/app/routes/privacy.ts
+++ src/app/routes/privacy.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: "privacy",
     templateUrl: "privacy.html"
 })
 export class PrivacyAndCookiesComponent {
+    
+    constructor(private translate: TranslateService) {}
+
+    get currentLang() { return this.translate.currentLang; }
 }
