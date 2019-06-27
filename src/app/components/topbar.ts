3a2550f3ce883457ceacc9485513ef2f4653c0c1
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2019-06-27 08:55:04 +0200

    translate topbar texts

--- src/app/components/topbar.ts
+++ src/app/components/topbar.ts
@@ -3,6 +3,7 @@ import { TypeWriter } from '../util/TypeWriter';
 import { AppService } from '../services/app';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { environment } from '../../environments/environment';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-topbar',
@@ -14,18 +15,20 @@ export class AppTopbarComponent implements OnInit {
 
     constructor(
         public app: AppService,
-        private snackBar: MatSnackBar
+        private snackBar: MatSnackBar,
+        private translate: TranslateService
     ) {}
 
-    ngOnInit() {
-        let tw = new TypeWriter(["„Was essen wir heute Mittag?“","lunch.community"], t => {
+    async ngOnInit() {
+        let slogan = await this.translate.get("components.topbar.slogan").toPromise();
+        let tw = new TypeWriter([slogan,"lunch.community"], t => {
             this.title = t;
         })
         tw.start();
     }
 
-    linkCopied() {
-        this.snackBar.open("Der Link zu dieser Community wurde in die Zwischenablage kopiert.", null, {duration: 3000});
+    async linkCopied() {
+        this.snackBar.open(await this.translate.get("components.topbar.link_copied").toPromise(), null, {duration: 3000});
     }
 
     get communityLink(): string {
