7990fdd38f4c8a29966a373f48c53d9f2835eea2
potpiejimmy <t.liese@outlook.com>
2020-11-22 16:16:11 +0100

    feat: upgraded API urls to reflect server change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.lunch.community/",
+  apiUrl: "https://api.lunch.community/api/v1/",
   shareUrl: "https://lunch.community",
   supportedLanguages: ["en","de"]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/",
+  apiUrl: "http://localhost:3000/api/v1/",
   shareUrl: "http://localhost:4200",
   supportedLanguages: ["en","de"]
 };
